c24a81f94f98ee4019e2530130507cbe969a1ca4
KentChen <bboykunz@gmail.com>
2024-01-30 19:24:46 +0800

    add component Square

---
+++ src/components/Square.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Square({ black }) {
+    const fill = black ? "black" : "white";
+    return (
+        <div style={{ backgroundColor: fill }}></div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Knight from './components/Knight'
+import Square from './components/Square'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Square black>
     <Knight />
+  </Square>
 )
