4516933cba6af6f7a827326b37caf8e5c6489a61
kunz <bboykunz@gmail.com>
2024-02-07 07:35:08 +0800

    fix renderSquare func and import react dnd and html5 backend

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,11 +5,16 @@ import Knight from "./Knight";
 //把移動騎士方法放在棋盤上
 import { moveKnight, canMoveKnight } from "./Game";
 
+//引入React dnd
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
 //棋盤元件
 
 //用此func來管理整面棋盤
 //原本的[knightX, knightY]改為Game來管理
 function renderSquare(i, knightPosition) {
+    const [knightX, knightY] = knightPosition;
     const x = i % 8;
     const y = Math.floor(i / 8);
     const black = (x + y) % 2 === 1;
@@ -18,9 +23,13 @@ function renderSquare(i, knightPosition) {
 
     return (
         <div key={i} style={{ width: "12.5%", height: "12.5%" }} 
-             onClick={() => handleSquareClick(x, y)}>
-            <Square black={black}>{piece}</Square>
-        </div>
+                onClick={() => handleSquareClick(x, y)}
+            >
+                <Square black={black}>{piece}</Square>
+            </div>
+        // <DndProvider backend={HTML5Backend}>
+            
+        // </DndProvider>
     );
 }
 
